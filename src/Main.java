eb475a8e8518f6bb6cf797233b9e483346616d7c
Meliksah Simsek <meliksah.simsek@aurea.com>
2020-01-12 12:09:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static int pow(int base, int power) {
+        int result = base;
+        for(int i = 0; i < power; i++) {
+            result *=base;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {{1,3,5,7},{0,0,6,6}, {0,2,1,3}, {5,3,1,1}, {1,0,1,1}};
+        int rows = 5;
+        int columns = 4;
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < rows; i++) {
+            int number = 0;
+            for(int j = 0; j < columns; j++) {
+                number += input[i][j] * (Math.pow(10, (columns - j -1)));
+            }
+            numbers.add(number);
+        }
+
+        for(Integer number: numbers) {
+            System.out.println(number);
+        }
+
+    }
+}
